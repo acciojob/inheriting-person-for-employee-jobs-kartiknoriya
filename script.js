22aab3ba57461cf43be98765cb4742e63529c718
kartiknoriya <148875248+kartiknoriya@users.noreply.github.com>
2026-01-14 19:16:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 // complete this js code
-function Person(name, age) {}
+class Person{
+      constructor(name,age){
+           this._name = name;
+           this._age = age;
+      } 
+      
+      get name(){
+            return this._name;
+      }
+      
+      get age(){
+            return  this._age;
+      }
+      
+      greet(){
+           console.log(`Hello, my name is ${this.name} I am  ${this.age} years old.`)
+      }         
+}
 
-function Employee(name, age, jobTitle) {}
+class Employee extends Person{
+         constructor(name , age , jobTitle){
+               super(name,age);
+               this._jobTitle= jobTitle;
+         }
+         get jobTitle(){
+               return  this._jobTitle;
+         }
+    
+   jobGreet(){
+           console.log(`Hello, my name is ${this.name} I am  ${this.age} years old,and my job title is ${this.jobTitle}.`)
+    }
+}
+
+const person = new Person("Alice", 25);
+person.greet();
+// Expected Output: Hello, my name is Alice, I am 25 years old.
+
+// Creating an instance of Employee
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
